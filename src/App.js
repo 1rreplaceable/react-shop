5f259607030767757ef2e5f4c9c8e422cc0c4200
Seok Jin <97454046+1rreplaceable@users.noreply.github.com>
2023-12-01 14:47:29 +0900

    react-shop

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from "react";
 import data from './data.js';
-import {Container, Nav, Navbar, Row, Col} from 'react-bootstrap'
+import {Container, Nav, Navbar, Row, Col, Button} from 'react-bootstrap'
 import {Routes, Route, Link, useNavigate} from 'react-router-dom'
 import Detail from './pages/Detail'
-function App() {
-    let [shoes] = useState(data)
-    let navigate = useNavigate(); // 페이지 이동을 도와줌
+import axios from "axios";
 
+function App() {
+    let [shoes,setShoes] = useState(data)
+    let navigate = useNavigate(); // 페이지 이동을 도와줌
+    let [clickCnt, setClickCnt] = useState(2);
+    let [isLoading, setIsLoading] = useState(false);
     return (
         <div className="App">
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
                     <Navbar.Brand href="#home">ReachShop</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
-                        <Nav.Link onClick={()=>{navigate('/detail')}}>Cart</Nav.Link>
+                        <Nav.Link onClick={() => {
+                            navigate('/')
+                        }}>Home</Nav.Link>
+                        <Nav.Link onClick={() => {
+                            navigate('/detail')
+                        }}>Cart</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -30,12 +37,28 @@ function App() {
                                 {
                                     shoes.map((a, i) => {
                                         return (
-                                            <Goods shoes={shoes[i]}></Goods>
+                                            <Goods shoes={shoes[i]} key={i}></Goods>
                                         )
                                     })
                                 }
                             </Row>
                         </Container>
+                        <Button onClick={() => {
+                            setClickCnt(clickCnt+1);
+                            setIsLoading(true);
+                            axios.get(`https://codingapple1.github.io/shop/data${clickCnt}.json`)
+                                .then((result) => {
+                                    let copy = [...shoes, ...result.data];
+                                    setShoes(copy);
+                                    setIsLoading(false);
+                                })
+                                .catch(() => {
+                                    setIsLoading(false);
+                                })
+                            // axios.post('/sad', {name :'kim'})
+                        }}>더보기
+                        </Button>
+                        <div className={isLoading?'loading':'hidden'}>로딩중입니다.</div>
                     </>
                 }/>
 
@@ -47,7 +70,7 @@ function App() {
 
 function Goods(props) {
     return (
-        <Col>
+        <Col md={4}>
             <img src={`https://codingapple1.github.io/shop/shoes${props.shoes.id + 1}.jpg`} width="80%"/>
             <h4>{props.shoes.title}</h4>
             <p>{props.shoes.price}</p>
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,27 +1,109 @@
-import {Col, Container, Row} from "react-bootstrap";
+import {Nav, Col, Container, Row} from "react-bootstrap";
 import {useNavigate, useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+
 
 function Detail(props) {
-
+    const [isVisible, setIsVisible] = useState(true);
+    let [count, setCount] = useState(0);
     let {id} = useParams();
     let sh = props.shoes.find((x) => {
         return x.id == id
+    });
+    let [inputValue, setInputValue] = useState('');
+    let [tab, setTab] = useState(0);
+    let [fadePage, setFadePage] = useState('');
+    useEffect(() => {
+        setTimeout(() => {
+            setFadePage('end');
+        }, 100)
+        return () => {
+            setFadePage('');
+        }
+    }, [])
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            // 2초 후에 className이 "alert"인 div를 숨기기
+            setIsVisible(false);
+        }, 2000);
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    }, []);
 
-    })
+    useEffect(() => {
+        if (isNaN(inputValue) === true) {
+            alert('숫자만 입력하세요!');
+        }
+    }, [inputValue])
+
 
     return (
-        <Container>
+        <Container className={'start '+fadePage}>
+            {/*<div className={isVisible ? 'alert alert-warning' : 'hidden'}>*/}
+            {/*    2초 이내 구매시 할인*/}
+            {/*</div>*/}
+            {/*<button onClick={() => {*/}
+            {/*    setCount(count + 1)*/}
+            {/*}}>버튼*/}
+            {/*</button>*/}
             <Row>
                 <Col>
                     <img src={`https://codingapple1.github.io/shop/shoes${sh.id + 1}.jpg`} width="80%"/>
+                    <input onChange={(e) => {
+                        setInputValue(e.target.value)
+                    }}/>
                     <h4 className="pt-5">{sh.title}</h4>
                     <p>{sh.content}</p>
                     <p>{sh.price}원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </Col>
             </Row>
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={() => {
+                        setTab(0)
+                    }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => {
+                        setTab(1)
+                    }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => {
+                        setTab(2)
+                    }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+
+            <TabContent tab={tab}/>
+
         </Container>
     );
 }
+
+function TabContent({tab}) {
+    let [fade, setFade] = useState('');
+    useEffect(() => {
+        setTimeout(() => {
+            setFade('end');
+        }, 100)
+        return () => {
+            setFade('');
+        }
+    }, [tab])
+
+    return (<div className={'start ' + fade}>
+        {[
+            <div>내용0</div>
+            ,
+            <div>내용1</div>
+            ,
+            <div>내용2</div>
+        ][tab]}
+    </div>)
+}
+
 
 export default Detail;
